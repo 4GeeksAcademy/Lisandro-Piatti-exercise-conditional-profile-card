cf41c984e913d0fb2f88b1a786ce432d2fcfa105
Lisandro Piatti <159830695+Lichatti@users.noreply.github.com>
2024-05-22 19:32:43 +0000

    solve the exercise

--- src/js/app.js
+++ src/js/app.js
@@ -28,15 +28,22 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  //comprobando que los valores no sean null
+  variables.name === null ? (variables.name = "Put your name") : null;
+  let yourLastName = variables.lastName === null ? "" : variables.lastName;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name} ${yourLastName}</h1>
+          <h2>${
+            variables.role === null ? (variables.role = "Role") : variables.role
+          }</h2>
+          <h3>${variables.city === null ? "city" : variables.city}, ${
+    variables.country === null ? "country" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
